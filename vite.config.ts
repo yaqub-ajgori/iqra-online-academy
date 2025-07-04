c5aa61a33c82521328454dba37c117bd7a4d4644
yaqub-ajgori <contact.yaqub@gmail.com>
2025-07-04 15:56:21 +0600

    vite updated

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
         tailwindcss(),
