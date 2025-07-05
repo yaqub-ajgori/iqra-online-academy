e3efeed93f7ad473913dc9561314edf86e91ce24
yaqub-ajgori <contact.yaqub@gmail.com>
2025-07-05 20:07:19 +0600

    seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(10)->create();
+        // User::factory()->count(10)->create();
 
         // admin user
         $admin = User::create([
